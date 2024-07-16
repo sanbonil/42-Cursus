09d67ee30ca108dfcf7fd782faa221f0c9207d8f
Sandra Bonillo <sanbonil@student.42barcelona.com>
2024-07-16 22:53:27 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -16,12 +16,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	len;
-
-	len = 0;
-	if (src != NULL)
-	{
-		len = ft_strlen((char *)src);
-		if (dst != NULL && dstsize != 0)
+	len = ft_strlen((char *)src);
+	if (dstsize != 0)
 		{
 			i = 0;
 			while ((i < len) && i < (dstsize - 1))
