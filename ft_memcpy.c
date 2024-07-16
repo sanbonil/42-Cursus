b618db454cd2c7c005f80150d7bbdf59d0ae85aa
Sandra Bonillo <sanbonil@student.42barcelona.com>
2024-07-16 22:52:40 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest || !src)
-		return (dest);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	while (n-- > 0)
